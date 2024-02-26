b21a7d22f863662b27b1ca240622be5a0f9d6dfe
Daniluxa <shumkin.daniil@mail.ru>
2024-02-26 14:53:59 +0300

    do lab7

--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -9,10 +9,7 @@ using namespace::std;
 template <typename T>
 void vectorPrint(vector<T> vec)
 {
-    for (int i = 0; i < vec.size(); ++i)
-    {
-        cout << vec[i] << " ";
-    }
+    for_each(vec.begin(), vec.end(), [](T& i) {cout << i << " "; });
     cout << endl;
 }
 
@@ -49,13 +46,41 @@ void task1()
 
 void task2()
 {
+    vector<float> accelarations(10);
+    vector<float> speed;
+    vector<float> distance_one_time;
+    vector<float> distance;
+    iota(accelarations.begin(), accelarations.end(), 0);
+    cout << "Создал массив ускорений при помощи iota" << endl;
+    vectorPrint(accelarations);
+
+    random_device rd;
+    mt19937 g(rd());
+    shuffle(accelarations.begin(), accelarations.end(), g);
+    cout << "Перемешал массив ускорений" << endl;
+    vectorPrint(accelarations);
+
+    partial_sum(accelarations.begin(), accelarations.end(), back_inserter(speed));
+    cout << "Скорость на каждом участке" << endl;
+    vectorPrint(speed);
+
+    partial_sum(speed.begin(), speed.end(), back_inserter(distance_one_time));
+    for_each(accelarations.begin(), accelarations.end(), [](float& i) {i /= 2; });
+    transform(accelarations.begin(), accelarations.end(), speed.begin(), distance_one_time.begin(), plus<float>());
+    cout << "Расстояния, пройденные за каждый промежуток времени" << endl;
+    vectorPrint(distance_one_time);
+
+    partial_sum(distance_one_time.begin(), distance_one_time.end(), back_inserter(distance));
+    cout << "Суммарное расстояние на каждой отметке" << endl;
+    vectorPrint(distance);
+    
 
 }
 
 int main()
 {
     setlocale(LC_ALL, "rus");
-    task1();
-    //task2();
+    //task1();
+    task2();
     return 0;
 }
